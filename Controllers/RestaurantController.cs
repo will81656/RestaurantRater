10c683151abba6e1e792e7fad4fbae69a1cacb8a
William casteel <will81656@gmail.com>
2021-03-22 20:42:14 -0500

    Delete

--- Controllers/RestaurantController.cs
+++ Controllers/RestaurantController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -33,6 +34,16 @@ namespace RestaurantRater.Controllers
 
             }
             return View(restaurant);
+        }
+        //Delete : Restaurant/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int? id)
+        {
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            _db.Restaurants.Remove(restaurant);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
